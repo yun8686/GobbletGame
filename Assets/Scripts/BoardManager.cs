60ce46d3adbba78bf490da39c1657ec3ab107718
Ryoga Komine <ryogakomine@Ryogas-MacBook-Pro.local>
2020-01-08 23:20:43 +0900

    ボード作成

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class BoardManager : MonoBehaviour
+{
+    private Camera camera_object;
+    private RaycastHit hit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera_object = GameObject.Find("Main Camera").GetComponent<Camera>();
+        int length = gameObject.transform.childCount;
+        for (int i = 0; i < length; ++i)
+        {
+            GameObject tile = gameObject.transform.GetChild(i).gameObject;
+            if (i % 2 == 0) // 縞模様にする
+            {
+                tile.GetComponent<Renderer>().material.color = Color.gray;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = camera_object.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+                GameObject clickedObject = hit.collider.gameObject;
+                //x,zの値を取得
+                int x = (int)clickedObject.transform.position.x;
+                int z = (int)clickedObject.transform.position.z;
+                Debug.Log(x.ToString() + " " + z.ToString());
+            }
+        }else if (Input.GetMouseButtonUp(0))
+        {
+        }
+    }
+}
---
+++ Assets/Scripts/Obake.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Obake : MonoBehaviour
+{
+    private Vector3 nowPos;
+
+    private bool isMoving = false;
+    private Camera mainCamera;
+    private RaycastHit hit;
+
+    // Use this for initialization
+    void Start()
+    {
+        mainCamera = Camera.main;
+        nowPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RayCheck();
+        }
+
+        if (isMoving)
+        {
+            MovePoisition();
+            if (Input.GetMouseButtonUp(0))
+            {
+                CommitPosition();
+                isMoving = false;
+            }
+        }
+
+    }
+
+    private void RayCheck()
+    {
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray.origin, ray.direction, out hit, Mathf.Infinity) && hit.collider == gameObject.GetComponent<Collider>())
+        {
+            isMoving = true;
+            gameObject.layer = LayerMask.NameToLayer("Ignore Raycast");
+        }
+        else
+        {
+            isMoving = false;
+            gameObject.layer = LayerMask.NameToLayer("Default");
+        }
+
+    }
+
+    private void MovePoisition()
+    {
+        transform.position = GetMousePosition();
+    }
+    private Vector3 GetMousePosition()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        Ray touchPointToRay = mainCamera.ScreenPointToRay(mousePos);
+        Vector3 p = nowPos;
+        if (Physics.Raycast(touchPointToRay, out hit))
+        {
+            p = hit.point;
+            p.y += transform.localScale.y;
+        }
+        return p;
+    }
+    private void CommitPosition()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        Ray touchPointToRay = mainCamera.ScreenPointToRay(mousePos);
+        if (Physics.Raycast(touchPointToRay, out hit))
+        {
+            GameObject tile = hit.collider.gameObject;
+            if (tile.CompareTag("Tile"))
+            {
+                nowPos = tile.transform.position;
+                nowPos.y = transform.localScale.y+hit.point.y;
+            }
+        }
+        transform.position = nowPos;
+    }
+}
